7dfcc48efe373569847b3f9c3d56f20f2a2cd765
MrTops <43430084+MrTops@users.noreply.github.com>
2021-01-02 14:13:28 -0500

    fixed token

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { ShardingManager } = require("discord.js");
+const { Token } = require('../Config.json');
+const { output } = require("./util/output");
 
-const shardingManager = new ShardingManager("./Bot.js");
+const shardingManager = new ShardingManager("./src/Bot.js", {
+	token: Token
+});
 
-shardingManager.on('shardCreate', async (shard) => {
-	
-})
+shardingManager.spawn().catch(reason => output);
